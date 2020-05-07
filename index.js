1843f68219e6a30127765b634f4799a49f798ac8
xiaomin zhu <xiaomin.zhu88@gmail.com>
2020-05-07 17:07:23 +0200

    Changed index without library

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-//const imageDownloader = require('node-image-downloader');
 
 const request = require('request');
 const cheerio = require('cheerio');
+
 const uri = 'https://memegen.link/examples';
+
+const dir = './memes';
+const fs = require('fs');
+
+!fs.existsSync(dir) && fs.mkdirSync(dir);
 
 request(uri, function (error, response, body) {
   if (!error && response.statusCode == 200) {
@@ -10,66 +15,22 @@ request(uri, function (error, response, body) {
     imgs = $('.meme-img').toArray();
 
     imgs
-      .filter((item, i) => i <= 10)
-      .forEach(function (img) {
+      .filter((item, i) => i < 10)
+      .forEach(function (img, i) {
         const url = 'https://memegen.link' + img.attribs.src;
 
         console.log(url);
+
+        const file = fs.createWriteStream(`./memes/image${i}.jpg`);
+
+        request
+          .get(url)
+          .on('err', (err) => {
+            console.log(err);
+          })
+          .pipe(file);
       });
   }
 });
 
-//imageDownloader({
-//  imgs: [
-//    {
-//      uri:
-//      'https://memegen.link/patrick/your_text/goes_here.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/success/your_text/goes_here.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/tenguy/your_text/goes_here.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/apcr/your_text/goes_here.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri: 'https://memegen.link/aag/_/aliens.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/tried/at_least/you_tried.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/biw/gets_iced_coffee/in_the_winter.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/blb/your_text/goes_here.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/ch/if_you_wanted_to_avoid_the_friend_zone///you_should_have_made_your_intentions_known_from_the_start.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//    {
-//      uri:
-//      'https://memegen.link/wonka/your_text/goes_here.jpg?preview=true&watermark=none',
-//      filename: 'meme-scraper',
-//    },
-//  ],
-//  dest: '/Users/mini/projects/meme-scraper',
-//});
+
---
+++ meme2.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const imageDownloader = require('node-image-downloader');
+
+
+request('https://memegen.link/examples', function (err, res, html) {
+  const $ = cheerio.load(html);
+  $('.row a').each((i, el) => {
+    const url = 'https://memegen.link';
+    const item = $(el).text();
+    const img = $(el).attr('href').replace(/[']/g, '').split('?')[0];
+    const link = url + img;
+    if (i < 10) {
+      const firstTen = link;
+      imageDownloader({
+        imgs: [
+          {
+            uri: link,
+          },
+        ],
+        dest: './memes',
+      });
+      console.log(firstTen);
+    }
+  });
+});
