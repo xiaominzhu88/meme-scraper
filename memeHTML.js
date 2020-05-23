61de19253efb85609f6c7d220419bf546874fd8c
xiaomin zhu <xiaomin.zhu88@gmail.com>
2020-05-23 07:52:27 +0200

    Delete unused dependencies

--- memeHTML.js
+++ memeHTML.js
@@ -1,15 +1,5 @@
 const cheerio = require('cheerio');
-//const fs = require('fs');
 const request = require('request');
-
-//const $ = cheerio.load('<img class="meme-img">...</img>');
-
-//request('https://memegen.link/examples', function (error, response, body) {
-//  console.error('error:', error);
-//  console.log('statusCode:', response && response.statusCode);
-//  console.log('body:', body);
-//});
-
 request(
   {
     method: 'GET',
@@ -17,9 +7,7 @@ request(
   },
   (err, res, body) => {
     if (err) return console.error(err);
-
     let $ = cheerio.load(body);
-
     console.log(body);
   },
 );
